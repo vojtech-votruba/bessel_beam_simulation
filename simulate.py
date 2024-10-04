df9de6bfb8dc8f1ea1987cbf62cc7670b4a0b4c5
Vojtěch Votruba <vv.votruba@gmail.com>
2024-10-04 16:53:48 +0200

    added better multiprocessing

--- simulate.py
+++ simulate.py
@@ -8,18 +8,47 @@ from numpy import angle
 from diffractio import np, plt, um, mm, degrees
 from diffractio.scalar_sources_XY import Scalar_field_XY
 from diffractio.utils_math import get_k
+from numpy.lib.scimath import sqrt as csqrt
+
 import pyfftw
 from pyfftw.interfaces.scipy_fftpack import fft2, fftshift, ifft2, convolve
-from numpy.lib.scimath import sqrt as csqrt
+from multiprocessing import Pool, cpu_count
+
+num_threads = cpu_count()
+pyfftw.config.NUM_THREADS = num_threads
+pyfftw.interfaces.cache.enable()
+
+def fft_per_row(row):
+    return fft2(row, overwrite_x=True)
+
+def ifft_per_row(row):
+    return ifft2(row, overwrite_x=True)
+
+def fftshift_per_row(row):
+    return fftshift(row)
+
+def fft_parallel_2d(matrix):
+    with Pool(processes=num_threads) as pool:
+        fft_rows = pool.map(fft_per_row, matrix)
+
+    return np.array(fft_rows)
+
+def ifft_parallel_2d(matrix):
+    with Pool(processes=num_threads) as pool:
+        ifft_rows = pool.map(ifft_per_row, matrix)
+
+    return np.array(ifft_rows)
+
+def fftshift_parallel_2d(matrix):
+    with Pool(processes=num_threads) as pool:
+        shift_rows = pool.map(fftshift_per_row, matrix)
+
+    return np.array(shift_rows)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--obstacle", action=argparse.BooleanOptionalAction,
                     default="True", help="Do you want to add the obstacle to the simulation?")
 args = parser.parse_args()
-
-num_threads = os.cpu_count()
-pyfftw.config.NUM_THREADS = num_threads
-pyfftw.interfaces.cache.enable()
 
 class Scalar_source_XY(Scalar_field_XY):
     """Class for XY scalar sources.
@@ -336,10 +365,10 @@ def PWD_kernel(u, n, k0, k_perp2, dz):
     """
     absorption = 0.00
 
-    Ek = fftshift(fft2(u))
-    # H = np.exp(1j * dz * csqrt(n**2 * k0**2 - k_perp2.transpose()) - absorption)
+    Ek = fftshift_parallel_2d(fft_parallel_2d(u))
     H = ne.evaluate("exp(1j * dz * sqrt(n**2 * k0**2 - k_perp2) - absorption)")
-    result = ifft2(fftshift(ne.evaluate("H * Ek")))
+    HEk = ne.evaluate("H * Ek")
+    result = ifft_parallel_2d(fftshift_parallel_2d(HEk))
     
     return result
 
@@ -434,152 +463,154 @@ def wpm_2d(x, y, u_field,
         
     return u_field
 
-"""
-All of the constants for this simulation are stored in the config.json file.
 
-The length dimensions used are inputed in millimeters,
-the energy is inputed in joules, length of the pulse is in seconds,
-wavelength is in um.
-"""
-with open("config.json", encoding="utf-8") as f:
-    CONSTANTS = json.load(f)
+if __name__ == "__main__":
+    """
+    All of the constants for this simulation are stored in the config.json file.
 
-W0 = CONSTANTS["laser"]["radius"]
-TOTAL_ENERGY = CONSTANTS["laser"]["total_energy"]
-TOTAL_SURFACE = CONSTANTS["laser"]["total_surface"]
-ENERGY = TOTAL_ENERGY / TOTAL_SURFACE * np.pi * W0**2
-AT = CONSTANTS["laser"]["pulse_length"]
-POWER = 2*np.sqrt(np.log(2)/np.pi) * ENERGY/AT
-I0 = POWER / (np.pi*W0**2)*100 # Intensity of the incoming plane wave in W/cm^2; I ~ E^2
-E0 = np.sqrt(I0) # Amplitude of the electric field in V/cm
+    The length dimensions used are inputed in millimeters,
+    the energy is inputed in joules, length of the pulse is in seconds,
+    wavelength is in um.
+    """
+    with open("config.json", encoding="utf-8") as f:
+        CONSTANTS = json.load(f)
 
-WAVELENGTH = CONSTANTS["laser"]["wavelength"] * um
-SCALE = 1/5
-REGION_SIZE = CONSTANTS["region"]["size"] * SCALE
+    W0 = CONSTANTS["laser"]["radius"]
+    TOTAL_ENERGY = CONSTANTS["laser"]["total_energy"]
+    TOTAL_SURFACE = CONSTANTS["laser"]["total_surface"]
+    ENERGY = TOTAL_ENERGY / TOTAL_SURFACE * np.pi * W0**2
+    AT = CONSTANTS["laser"]["pulse_length"]
+    POWER = 2*np.sqrt(np.log(2)/np.pi) * ENERGY/AT
+    I0 = POWER / (np.pi*W0**2)*100 # Intensity of the incoming plane wave in W/cm^2; I ~ E^2
+    E0 = np.sqrt(I0) # Amplitude of the electric field in V/cm
 
-Nx = int(1000*REGION_SIZE)
-Ny = int(1000*REGION_SIZE)
+    WAVELENGTH = CONSTANTS["laser"]["wavelength"] * um
+    SCALE = 1/5
+    REGION_SIZE = CONSTANTS["region"]["size"] * SCALE
 
-print(f"In the x axis using {Nx/REGION_SIZE/1000} px/um with total of {Nx} px")
-print(f"In the y axis using {Ny/REGION_SIZE/1000} px/um with total of {Ny} px")
+    Nx = int(1000*REGION_SIZE)
+    Ny = int(1000*REGION_SIZE)
 
-x = np.linspace(-REGION_SIZE/2*mm, REGION_SIZE/2*mm, Nx,)
-y = np.linspace(-REGION_SIZE/2*mm, REGION_SIZE/2*mm, Ny,)
-print(f"Available memory: {psutil.virtual_memory().available/1000000000} GB")
-print(f"The number of threads is {num_threads}\n")
+    print(f"In the x axis using {Nx/REGION_SIZE/1000} px/um with total of {Nx} px")
+    print(f"In the y axis using {Ny/REGION_SIZE/1000} px/um with total of {Ny} px")
 
-
-# Initiation of the field
-def initialization(x_, y_, wavelength):
-    u0 = Scalar_source_XY(x_, y_, wavelength)
-    u0.plane_wave(A=E0, theta=0*degrees)
-
-    t0 = Scalar_mask_XY(x_, y_, wavelength)
-    t0.circle(r0=(0*mm, 0*mm),
-            radius=REGION_SIZE/2*mm - 5*SCALE*mm,
-            angle=0)
-
-    t1 = Scalar_mask_XY(x_, y_, wavelength)
-    t1.axicon(r0=(0*mm, 0*mm),
-            radius=REGION_SIZE/2*mm - 2*SCALE*mm,
-            angle=CONSTANTS["axicon"]["angle"]*degrees,
-            refractive_index=1.51,
-            reflective=True)
-
-    return np.complex64((u0 * t0 * t1).u)
-
-start = time.time()
-u_new = initialization(x, y, WAVELENGTH)
-length = time.time() - start
-
-print(f"The field is initialized, it took {length} sec.")
-print(f"Available memory after initialization: {psutil.virtual_memory().available/1000000000} GB\n")
-
-""" Field before the obstacle using RS
-u_new = u1.RS(z=(CONSTANTS["nozzle"]["dist_z"]-5)* mm, verbose=True)
-u_cut = u_new#.cut_resample([-10,10], [-10,10], num_points=(128,128), new_field=True)
-u_cut.draw(kind="intensity", title=f"xy profile in {CONSTANTS['nozzle']['dist_z']-5} mm using Rayleigh-Sommerfeld method")
-u_new = u_new.u"""
-
-start = time.time()
-# Propagation through the obstacle
-PROFILE_LOCATIONS = [50*mm*SCALE, 100*mm*SCALE]
+    x = np.linspace(-REGION_SIZE/2*mm, REGION_SIZE/2*mm, Nx,)
+    y = np.linspace(-REGION_SIZE/2*mm, REGION_SIZE/2*mm, Ny,)
+    print(f"Available memory: {psutil.virtual_memory().available/1000000000} GB")
+    print(f"The number of threads is {num_threads}\n")
 
 
-for seq,location in enumerate(PROFILE_LOCATIONS):
-    print(f"Calulating the {seq+1}/{len(PROFILE_LOCATIONS)} profile with WPM")
+    # Initiation of the field
+    def initialization(x_, y_, wavelength):
+        u0 = Scalar_source_XY(x_, y_, wavelength)
+        u0.plane_wave(A=E0, theta=0*degrees)
 
-    if seq == 0:
-        #distance = location - (CONSTANTS["nozzle"]["dist_z"]-5)* mm
-        distance = location
-    else:
-        distance = location - PROFILE_LOCATIONS[seq-1]
+        t0 = Scalar_mask_XY(x_, y_, wavelength)
+        t0.circle(r0=(0*mm, 0*mm),
+                radius=REGION_SIZE/2*mm - 5*SCALE*mm,
+                angle=0)
 
-    Nz = int(distance/1000)
-    print(f"In the z axis using {Nz/distance} px/um with total of {Nz} px")
+        t1 = Scalar_mask_XY(x_, y_, wavelength)
+        t1.axicon(r0=(0*mm, 0*mm),
+                radius=REGION_SIZE/2*mm - 2*SCALE*mm,
+                angle=CONSTANTS["axicon"]["angle"]*degrees,
+                refractive_index=1.51,
+                reflective=True)
 
-    if seq == 0:
-        #u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, (CONSTANTS["nozzle"]["dist_z"]-5)*mm, location, args.obstacle)
-        u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, 0*mm, location, args.obstacle)
+        return np.complex64((u0 * t0 * t1).u)
 
-    else:
-        u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, PROFILE_LOCATIONS[seq-1], location, args.obstacle)
-    
-    plt.figure()
-    #plt.colorbar(plt.cm.ScalarMappable(cmap="hot", norm=None) , label="I (W/cm^2)")
-    plt.title(f"xy profile in {location/1000} mm")
-    plt.xlabel("y (mm)")
-    plt.ylabel("x (mm)")
+    start = time.time()
+    u_new = initialization(x, y, WAVELENGTH)
+    length = time.time() - start
 
-    plt.imshow(abs(u_new)**2,
-        cmap="hot",
-        aspect="equal",
-        extent=(-REGION_SIZE/2, REGION_SIZE/2, -REGION_SIZE/2, REGION_SIZE/2),)
+    print(f"The field is initialized, it took {length} sec.")
+    print(f"Available memory after initialization: {psutil.virtual_memory().available/1000000000} GB\n")
 
-length = time.time() - start
-print(f"\nCalculating profiles took {length} sec")
+    """ Field before the obstacle using RS
+    u_new = u1.RS(z=(CONSTANTS["nozzle"]["dist_z"]-5)* mm, verbose=True)
+    u_cut = u_new#.cut_resample([-10,10], [-10,10], num_points=(128,128), new_field=True)
+    u_cut.draw(kind="intensity", title=f"xy profile in {CONSTANTS['nozzle']['dist_z']-5} mm using Rayleigh-Sommerfeld method")
+    u_new = u_new.u"""
 
-plt.show()
+    start = time.time()
+    # Propagation through the obstacle
+    PROFILE_LOCATIONS = [50*mm*SCALE, 100*mm*SCALE]
 
-"""
-uxyz = Scalar_mask_XYZ(x, y, z, WAVELENGTH)
 
-if args.obstacle:
-    # Upper, declined part of the nozzle
-    uxyz.prism(r0=(0,(CONSTANTS["nozzle"]["dist_x"] - CONSTANTS["nozzle"]["slope_height"])*mm,
-                            (CONSTANTS["nozzle"]["z_size"]/2 + CONSTANTS["nozzle"]["dist_z"])*mm),
-                            length = CONSTANTS["nozzle"]["z_size"]*mm,
-                            refractive_index=1.2+7j, # Approximately the refractive index of aluminum
-                            height=CONSTANTS["nozzle"]["slope_height"]*mm,
-                            upper_base=CONSTANTS["nozzle"]["slope_upper"]*mm,
-                            lower_base=CONSTANTS["nozzle"]["slope_lower"]*mm)
+    for seq,location in enumerate(PROFILE_LOCATIONS):
+        print(f"Calulating the {seq+1}/{len(PROFILE_LOCATIONS)} profile with WPM")
 
-    # Lower part of the nozzle
-    uxyz.square(r0=(0,(-(REGION_SIZE/4) + CONSTANTS["nozzle"]["dist_x"] - CONSTANTS["nozzle"]["slope_height"])*mm,
-                (CONSTANTS["nozzle"]["z_size"]/2 + CONSTANTS["nozzle"]["dist_z"])*mm), # the X and Y coordinates are inverted for some reason.
-                length=(CONSTANTS["nozzle"]["y_size"]*mm,
-                        (REGION_SIZE/2 + CONSTANTS["nozzle"]["dist_x"])*mm + 5*mm,
-                        CONSTANTS["nozzle"]["z_size"]*mm),
-                refractive_index=1.2+7j, # Approximately the refractive index of aluminum
-                angles=(0*degrees, 0*degrees, 0*degrees),
-                rotation_point=0)
+        if seq == 0:
+            #distance = location - (CONSTANTS["nozzle"]["dist_z"]-5)* mm
+            distance = location
+        else:
+            distance = location - PROFILE_LOCATIONS[seq-1]
 
-uxyz.incident_field(u2)
-uxyz.clear_field()
-uxyz.WPM(verbose=True, has_edges=True)
+        Nz = int(distance/1000)
+        print(f"In the z axis using {Nz/distance} px/um with total of {Nz} px")
 
-uxyz.draw_XZ(y0=0, kind='intensity',
-            normalize=False,
-            colorbar_kind='vertical',
-            draw_borders = True,)
+        if seq == 0:
+            #u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, (CONSTANTS["nozzle"]["dist_z"]-5)*mm, location, args.obstacle)
+            u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, 0*mm, location, args.obstacle)
 
-XY_PROFILES = [50, 130]
+        else:
+            u_new = wpm_2d(x, y, u_new, Nz, WAVELENGTH, PROFILE_LOCATIONS[seq-1], location, args.obstacle)
+        
+        plt.figure()
+        #plt.colorbar(plt.cm.ScalarMappable(cmap="hot", norm=None) , label="I (W/cm^2)")
+        plt.title(f"xy profile in {location/1000} mm")
+        plt.xlabel("y (mm)")
+        plt.ylabel("x (mm)")
 
-for z in XY_PROFILES:
-    #XY_cut = uxyz.cut_resample([-10,10], [-10,10], num_points=(128,128,128), new_field=True)
-    uxyz.draw_XY(z0=z*mm, kind='intensity',
-                title=f"XY profile in {z} mm",
+        plt.imshow(abs(u_new)**2,
+            cmap="hot",
+            aspect="equal",
+            extent=(-REGION_SIZE/2, REGION_SIZE/2, -REGION_SIZE/2, REGION_SIZE/2),)
+
+    length = time.time() - start
+    print(f"\nCalculating profiles took {length} sec")
+
+    plt.show()
+
+    """
+    uxyz = Scalar_mask_XYZ(x, y, z, WAVELENGTH)
+
+    if args.obstacle:
+        # Upper, declined part of the nozzle
+        uxyz.prism(r0=(0,(CONSTANTS["nozzle"]["dist_x"] - CONSTANTS["nozzle"]["slope_height"])*mm,
+                                (CONSTANTS["nozzle"]["z_size"]/2 + CONSTANTS["nozzle"]["dist_z"])*mm),
+                                length = CONSTANTS["nozzle"]["z_size"]*mm,
+                                refractive_index=1.2+7j, # Approximately the refractive index of aluminum
+                                height=CONSTANTS["nozzle"]["slope_height"]*mm,
+                                upper_base=CONSTANTS["nozzle"]["slope_upper"]*mm,
+                                lower_base=CONSTANTS["nozzle"]["slope_lower"]*mm)
+
+        # Lower part of the nozzle
+        uxyz.square(r0=(0,(-(REGION_SIZE/4) + CONSTANTS["nozzle"]["dist_x"] - CONSTANTS["nozzle"]["slope_height"])*mm,
+                    (CONSTANTS["nozzle"]["z_size"]/2 + CONSTANTS["nozzle"]["dist_z"])*mm), # the X and Y coordinates are inverted for some reason.
+                    length=(CONSTANTS["nozzle"]["y_size"]*mm,
+                            (REGION_SIZE/2 + CONSTANTS["nozzle"]["dist_x"])*mm + 5*mm,
+                            CONSTANTS["nozzle"]["z_size"]*mm),
+                    refractive_index=1.2+7j, # Approximately the refractive index of aluminum
+                    angles=(0*degrees, 0*degrees, 0*degrees),
+                    rotation_point=0)
+
+    uxyz.incident_field(u2)
+    uxyz.clear_field()
+    uxyz.WPM(verbose=True, has_edges=True)
+
+    uxyz.draw_XZ(y0=0, kind='intensity',
                 normalize=False,
-                has_colorbar=True,) # Doesn't work for some stupid reason
+                colorbar_kind='vertical',
+                draw_borders = True,)
 
-plt.show()"""
+    XY_PROFILES = [50, 130]
+
+    for z in XY_PROFILES:
+        #XY_cut = uxyz.cut_resample([-10,10], [-10,10], num_points=(128,128,128), new_field=True)
+        uxyz.draw_XY(z0=z*mm, kind='intensity',
+                    title=f"XY profile in {z} mm",
+                    normalize=False,
+                    has_colorbar=True,) # Doesn't work for some stupid reason
+
+    plt.show()"""
